d02e7613e7572813e34cec5f0c49be20519883df
mar1n <30738216+mar1n@users.noreply.github.com>
2021-01-21 13:26:53 +0000

    connect to socket.io

--- server.js
+++ server.js
@@ -7,5 +7,56 @@ mongo.connect('mongodb://127.0.0.1/mongochat', {useNewUrlParser: true, useUnifie
         throw err;
     }
 
-    console.log('MongoDb connected...')
+    console.log('MongoDb connected...');
+
+    //connect to sokect.io
+    client.on('connection', function() {
+        let chat = db.collection('chats');
+
+        // Create function to send status
+        sendStatus = function(s) {
+            socket.emit('status', s);
+        }
+
+        // Get chats from mongo collections
+        chat.find().limit(100).sort({_id: 1}).toArray(function(err, res) {
+            if(err) {
+                throw err;
+            }
+
+            // Emit the messages
+            socket.emit('output', res);
+        })
+
+        // Handle input events
+        socket.on('input', function(data) {
+            let name = data.name;
+            let message = data.message;
+
+            // check for name or message
+            if(name === '' || message === '') {
+                //Send  error status
+                sendStatus('Please enter a name and message')
+            } else {
+                // Insert message
+                chat.insert({name: name, message: message}, function() {
+                    client.emit('output', [data]);
+
+                    sendStatus({
+                        message: 'Message, sent',
+                        clear: true
+                    })
+                })
+            }
+        })
+
+        // Handel clear
+        socket.on('clear', function(data) {
+            // Remove all chats from collection
+            chat.remove({}, function() {
+                // Emit cleared
+                socket.emit('clear')
+            })
+        })
+    })
 })
